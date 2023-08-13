a4814acc783663f408893e114de61c9ada785298
Cory <corymclean0@gmail.com>
2023-08-13 15:02:46 -0400

    Employee

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee={name:"Cory",
+streetAddress:"123 Alabaster Rd"}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return obj}
+
+function deleteFromEmployeeByKey(employee,key){
+    const newObj={...employee};
+    delete newObj[key]
+return newObj}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+return employee}
